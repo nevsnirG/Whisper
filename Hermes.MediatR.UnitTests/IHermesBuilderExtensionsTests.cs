01f194af180ac2d5d5e64d63e999d879d6682f4b
Ken <ken@vgss.io>
2025-08-26 16:37:33 +0200

    Add MediatR tests

---
+++ Hermes.MediatR.UnitTests/IHermesBuilderExtensionsTests.cs
@@ -0,0 +1,18 @@
+﻿using Hermes.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hermes.MediatR.UnitTests;
+
+public class IHermesBuilderExtensionsTests
+{
+    [Fact]
+    public void AddMediatR_RegistersDispatcher()
+    {
+        var serviceCollection = new ServiceCollection() as IServiceCollection;
+        serviceCollection.AddHermes(b => b.AddMediatR());
+
+        var registration = serviceCollection.Should().ContainSingle().Which;
+        registration.ServiceType.Should().Be<IDispatchDomainEvents>();
+        registration.ImplementationType.Should().Be<MediatorDispatcher>();
+    }
+}
